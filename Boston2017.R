5958d03a1d879fc48c07c2b30bbede167c2f9780
Jreichenstein <jreichenstein@gmail.com>
2018-04-22 20:44:35 -0700

    looking at Runners by city

--- Boston2017.R
+++ Boston2017.R
@@ -1,6 +1,7 @@
 library(readr)
 library(tidyr)
 library(dplyr)
+library(ggplot2)
 
 res <- read_csv("marathon_results_2017.csv")
 
@@ -22,7 +23,11 @@ res_USA <- res_clean %>% filter(Country == "USA")
 res_by_city <- group_by(res_USA, City)
 
 #How many runners came from each city
-count(res_by_city) %>% arrange(desc(n))
+runners_by_city <- count(res_by_city) %>% arrange(desc(n))
+
+runners_by_city_top <- runners_by_city[1:10,]
+
+ggplot(runners_by_city_top, aes(x=as.factor(City), y = n)) + geom_bar(stat = "identity") + xlab("City") + ylab("Number of Runners") + ggtitle("Runners in the Boston Marathon", subtitle = "by city") 
 
 #Generating city avg, fastest, and slowest times
 avg_time_city <- res_by_city %>% summarise(avg_finish_time = mean(`Official Time`), fastest_finish_time = min(`Official Time`), slowest_finish_time = max(`Official Time`)) %>% arrange(avg_finish_time) %>% arrange(fastest_finish_time)
